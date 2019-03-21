7fc095d083d7f23515c6fdc8b4a2694755e91a49
vkensou <boywanyx@163.com>
2019-03-21 22:48:00 +0800

    完善四元数计算

--- Src/Math/Quaternion.h
+++ Src/Math/Quaternion.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Vector3.h"
+
 class Quaternion
 {
 public:
@@ -11,5 +13,12 @@ public:
 	float z{ 0 };
 	float w{ 0 };
 
+	Quaternion operator*(const Quaternion& rhs);
+	Vector3 operator*(const Vector3& v);
+
+	static Quaternion Euler(float x, float y, float z);
+	static Quaternion AxisAngle(Vector3 axis, float angle);
+
+	static const Quaternion identity;
 private:
 };
